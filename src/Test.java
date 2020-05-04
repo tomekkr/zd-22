3b9797636d100cea78d4cd42c87da942ffb5572e
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-05-04 21:28:26 +0200

    deleted Input class

--- src/Test.java
+++ src/Test.java
@@ -4,13 +4,13 @@ class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Input userInput = new Input();
-        Input lastInput = new Input();
+        String userInput = null;
+        String lastInput;
 
         System.out.println("Start: ");
         do {
-            lastInput.setString(userInput.getString());
-            userInput.setString(sc.nextLine());
+            lastInput = userInput;
+            userInput = sc.nextLine();
         } while (!userInput.equals(lastInput));
 
         sc.close();
